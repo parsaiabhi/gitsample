09e16491507d13cc1654bf8870bbf4c00634eb17
abhishek <tech.parsai@gmail.com>
2022-04-27 12:35:26 +0530

    my Initial commit

---
+++ src/test/java/com/example/gitsample/gitsample/GitsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitsample.gitsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
