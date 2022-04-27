e05c4570890f0d6ceda1704c057d4b78b61cb164
abhishek <tech.parsai@gmail.com>
2022-04-27 13:54:51 +0530

    "domain class field added"

--- src/main/java/com/example/gitsample/gitsample/domain/Person.java
+++ src/main/java/com/example/gitsample/gitsample/domain/Person.java
@@ -1,4 +1,32 @@
 package com.example.gitsample.gitsample.domain;
 
 public class Person {
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Person{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", address='").append(address).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
